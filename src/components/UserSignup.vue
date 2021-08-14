5784a6ecc71a37f92f551c92cbd72ff5b55a4ea8
Awanish Kumar <57137714+Awanish22@users.noreply.github.com>
2021-08-14 10:29:40 +0530

    login with laravel

---
+++ src/components/UserSignup.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <div class="container">
+    <h4>User Signup</h4>
+    <form @submit="submitData" method="post" >
+      <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Name</label>
+    <input type="text" name="name" class="form-control" id="exampleInputName" v-model="post.name" aria-describedby="namelHelp">
+    
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" name="email" class="form-control" v-model="post.email" id="exampleInputEmail1" aria-describedby="emailHelp">
+    
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" name="password" class="form-control" v-model="post.password" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+    
+    
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+    </div>
+<router-view></router-view>
+  </div>
+</template>
+
+<script>
+import  Vue from 'vue' 
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios,axios)
+
+export default {
+   name: 'Signup',
+  methods:{
+    submitData(e)
+    {
+      this.axios.post('http://localhost:8000/api/login',this.post).then((result)=>{console.warn("result",result)})
+      console.warn(this.post)
+      e.preventDefault();
+    //   console.log("submitData",this.SignupForm.username,this.SignupForm.useremail,this.SignupForm.userpassword)
+    },
+    
+  },
+  data()
+    {
+      return {
+          post:{
+        name:null,
+        email:null,
+        password:null,
+      }}
+    }
+}
+</script>
+
+<style>
+
+</style>
